2b33922142a98d170f06b1a2ca9c497d6be8f9ae
Oscar Blazejewski* <oscar_blazejewski@yahoo.fr>
2018-11-08 00:16:16 +0100

    Tartine

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from keras.models import load_model
 
 def make_model():
     input_all = Input(shape=(52,), name="input")
-    x = Dense(256, activation='relu')(input_all)
-    x = Dense(512, activation='relu')(x)
-    x = Dense(256)(x)
-    out = Dense(32)(x)
+    x = Dense(128, activation='relu')(input_all)
+    x = Dense(256, activation='relu')(x)
+    x = Dense(128, activation='relu')(x)
+    out = Dense(32, activation='relu')(x)
     model = Model(inputs=[input_all], outputs=out)
     model.compile(optimizer='rmsprop', loss='mse')
     return model
@@ -23,7 +23,7 @@ def make_model():
 # change_combinaison: int 0-31 mapped to [X X X X X]
 
 
-def __map_to_draw(combinaison):
+def map_to_draw(combinaison):
     vec = [int(x) for x in list('{0:0b}'.format(combinaison))]
     while len(vec) < 5:
         vec.insert(0, 0)
@@ -61,7 +61,7 @@ def hand_to_indexes(hand):
 
 
 def draw(hand, change_combinaison):
-    mapped_combinaison = __map_to_draw(change_combinaison)
+    mapped_combinaison = map_to_draw(change_combinaison)
     for i in range(5):
         if mapped_combinaison[i] == 1:
             hand[i] = deck.draw(1)
@@ -70,23 +70,28 @@ def draw(hand, change_combinaison):
 deck = Deck()
 evaluator = Evaluator()
 model = make_model()
-num_hands = 50000
-train_steps = 2000
-max_replay = 5000
-epsi = 0.05
+num_hands = 1000000
+train_steps = 20000
+max_replay = 50000
+epsi = 1
+metrics_window = 10000
 
 train_X = []
 train_Y = []
 
+metrics_score = []
+current_metric_score = 0
 for i in range(num_hands):
 
     deck.shuffle()
     hand = deck.draw(5)
+    if (i+1) % train_steps == 0:
+        old_hand = hand.copy()
     old_eval = evaluator.evaluate([], hand)
 
     feat = hand_to_indexes(hand)
+    Q = model.predict(feat.reshape(1, 52))
     if random.random() > epsi:
-        Q = model.predict(feat.reshape(1, 52))
         action = np.argmax(Q)
     else:
         action = random.randint(0, 31)
@@ -95,6 +100,10 @@ for i in range(num_hands):
     new_eval = evaluator.evaluate([], hand)
 
     reward = get_reward(old_eval, new_eval)
+    current_metric_score += reward
+    if (i+1) % metrics_window == 0:
+        metrics_score.append(current_metric_score/metrics_window)
+        current_metric_score = 0
     train_X.append(feat.reshape((52,)))
 
     Q[0, action] = reward
@@ -102,9 +111,22 @@ for i in range(num_hands):
 
     if (i+1) % train_steps == 0:
         print("hand n° ", str(i))
+        print("random %", str(epsi))
+        print(Card.print_pretty_cards(old_hand))
+        print(map_to_draw(action))
         print(Card.print_pretty_cards(hand))
-        model.fit(np.stack(train_X), np.stack(train_Y), verbose=1, epochs=5)
+        print(Q)
+        model.fit(np.stack(train_X), np.stack(train_Y), verbose=1, epochs=2)
+
+        # reduce explo
+        if epsi > 0.02:
+            epsi -= 0.02
 
     if len(train_X) > max_replay:
         train_X = train_X[50:]
         train_Y = train_Y[50:]
+
+print(metrics_score)
+print("Saving model..")
+model.save('models/model1.h5')
+print("Model saved to models/model1.h5..")
